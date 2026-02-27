51f4e9e1c97dfd623c3d485739d5627b0ebfb437
UIT2024002 <tanaya.bhoyar@cumminscollege.in~>
2026-02-27 11:23:21 +0530

    Initial Spring Boot backend setup

---
+++ backend/src/main/java/com/spectacles/spectacles/SpectaclesApplication.java
@@ -0,0 +1,13 @@
+package com.spectacles.spectacles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpectaclesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpectaclesApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/spectacles/spectacles/SpectaclesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spectacles.spectacles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpectaclesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
