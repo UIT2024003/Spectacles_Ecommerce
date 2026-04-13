519c21716c8a8d7fc508189fc4a4d4a41dc7b39d
Tanvi-Shirwadkar <tanvi.shirwadkar13@gmail.com>
2026-04-13 18:02:23 +0530

    To store image in db

--- backend/src/main/java/com/spectacles/spectacles/controller/ProductController.java
+++ backend/src/main/java/com/spectacles/spectacles/controller/ProductController.java
@@ -1,9 +1,11 @@
 package com.spectacles.spectacles.controller;
 
 import java.util.List;
+import java.io.File;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.spectacles.spectacles.model.Product;
 import com.spectacles.spectacles.service.ProductService;
@@ -26,9 +28,53 @@ public class ProductController {
     }
 
     // ✅ ADD PRODUCT
-    @PostMapping
+    /* @PostMapping
     public Product addProduct(@RequestBody Product product) {
         return productService.addProduct(product);
+    } */
+
+    @PostMapping("/upload")
+    public Product addProduct(
+            @RequestParam("file") MultipartFile file,
+            @RequestParam("name") String name,
+            @RequestParam("brand") String brand,
+            @RequestParam("price") double price,
+            @RequestParam("category") String category,
+            @RequestParam("description") String description,
+            @RequestParam("stock") int stock
+    ) {
+        try {
+
+            if (file == null || file.isEmpty()) {
+                throw new RuntimeException("File is missing!");
+            }
+
+            String fileName = file.getOriginalFilename();
+
+            String uploadDir = System.getProperty("user.dir") + "/uploads/";
+            File uploadPath = new File(uploadDir);
+
+            if (!uploadPath.exists()) {
+                uploadPath.mkdirs();
+            }
+
+            file.transferTo(new File(uploadDir + fileName));
+
+            Product product = new Product();
+            product.setName(name);
+            product.setBrand(brand);
+            product.setPrice(price);
+            product.setCategory(category);
+            product.setDescription(description);
+            product.setStock(stock);
+            product.setImageUrl(fileName);
+
+            return productService.addProduct(product);
+
+        } catch (Exception e) {
+            e.printStackTrace(); // VERY IMPORTANT
+            throw new RuntimeException(e);
+        }
     }
 
     // ✅ GET PRODUCT BY ID
